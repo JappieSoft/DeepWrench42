7ccdc4ff81f691351d93df619fea16d99cddef10
JappieSoft <j.aeromaint@gmail.com>
2026-02-26 17:49:37 +0100

    continueing with controller functions and added more expection handling

--- src/main/java/nl/novi/deepwrench42/controllers/UserController.java
+++ src/main/java/nl/novi/deepwrench42/controllers/UserController.java
@@ -3,7 +3,9 @@ package nl.novi.deepwrench42.controllers;
 import jakarta.validation.Valid;
 import nl.novi.deepwrench42.dtos.user.UserRequestDTO;
 import nl.novi.deepwrench42.dtos.user.UserResponseDTO;
+import nl.novi.deepwrench42.exceptions.DuplicateFieldException;
 import nl.novi.deepwrench42.helpers.UrlHelper;
+import nl.novi.deepwrench42.repository.UserRepository;
 import nl.novi.deepwrench42.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +18,12 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
     private final UrlHelper urlHelper;
 
-    public UserController(UserService userService, UrlHelper urlHelper) {
+    public UserController(UserService userService, UserRepository userRepository, UrlHelper urlHelper) {
         this.userService = userService;
+        this.userRepository = userRepository;
         this.urlHelper = urlHelper;
     }
 
@@ -40,6 +44,16 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userInput) {
+        if (userRepository.existsByEmployeeId(userInput.getEmployeeId())) {
+            throw new DuplicateFieldException("Employee ID already exists");
+        }
+        if (userRepository.existsBySchipholId(userInput.getSchipholId())) {
+            throw new DuplicateFieldException("Schiphol ID already exists");
+        }
+        if (userRepository.existsByEmail(userInput.getEmail())) {
+            throw new DuplicateFieldException("Email already exists");
+        }
+
         UserResponseDTO newUser = userService.createUser(userInput);
         return ResponseEntity.created(urlHelper.getCurrentUrlWithId(newUser.getId())).body(newUser);
     }
--- src/main/java/nl/novi/deepwrench42/exceptions/GlobalExceptionHandler.java
+++ src/main/java/nl/novi/deepwrench42/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package nl.novi.deepwrench42.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.postgresql.util.PSQLException;
 
 import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -41,4 +44,31 @@ public class GlobalExceptionHandler {
         return ex.getMessage();
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDataIntegrity(DataIntegrityViolationException ex) {
+        var root = ex.getRootCause();
+        if (root instanceof PSQLException psql) {
+            var msg = psql.getMessage();
+            if (msg.contains("users_employee_id_key")) {
+                return ("Employee ID already exists");
+            }
+            if (msg.contains("users_schiphol_id_key")) {
+                return ("Schiphol ID already exists");
+            }
+            if (msg.contains("users_email_key")) {
+                return ("Email already exists");
+            }
+            if (msg.contains("violates foreign key constraint")) {
+                return ("Unable to delete: requested ID is still in use");
+            }
+        }
+        return ("Data integrity violation");
+    }
+
+    @ExceptionHandler(DuplicateFieldException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDuplicate(DuplicateFieldException ex) {
+        return ex.getMessage();
+    }
 }
